febd3eec548f5be895bf580db2bf74ce53c70e20
seet61 <seet_seet@mail.ru>
2018-04-16 22:46:05 +0300

    fix script for cron

--- run.py
+++ run.py
@@ -12,54 +12,24 @@ from subprocess import Popen
 
 pid_file = '/root/.bitcoinz/zcashd.pid'
 script = '/root/bitcoinz/src/zcashd'
+script_service = 'service znomp restart'
 
-class MyService(Service):
-    def __init__(self, *args, **kwargs):
-        super(MyService, self).__init__(*args, **kwargs)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        rfh = RotatingFileHandler('logger.log', mode='a', maxBytes=1024, backupCount=10, encoding='utf8', delay=0)
-        rfh.setFormatter(formatter)
-        self.logger.addHandler(rfh)
-        self.logger.setLevel(logging.INFO)
+def read_pid():
+    with open(pid_file, 'r') as data:
+        for line in data:
+            return line[:-1]
 
-    def read_pid(self):
-        with open(pid_file, 'r') as data:
-            for line in data:
-                return line[:-1]
-
-    def main(self):
-        pid = self.read_pid()
-        self.logger.info("pid: " + pid + " "  + str(os.path.exists("/proc/"+pid)))
-        if (os.path.exists("/proc/"+pid) == False):
-            self.logger.info('start service')
-            p = Popen(script, shell=True)
-            p.communicate()
-
-    def run(self):
-        self.logger.info('#'*80)
-        self.logger.info('starting')
-        schedule.every(1).minutes.do(self.main)
-        while not self.got_sigterm():
-            schedule.run_pending()
-        self.logger.info('stoped')
+def main():
+    pid = read_pid()
+    logging.basicConfig(filename=os.getcwd() +'checker.log', level=DEBUG, format='%(asctime)-15s %(levelname)s %(message)s')
+    logging.info("start")
+    if (os.path.exists("/proc/"+pid) == False):
+        logging.info('start service')
+        p = Popen(script, shell=True)
+        p.communicate()
+        p = Popen(script_service, shell=True)
+        p.communicate()
+    logging.info("stop")        
 
 if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) != 2:
-        sys.exit('Syntax: %s COMMAND' % sys.argv[0])
-
-    cmd = sys.argv[1].lower()
-    service = MyService('my_service', pid_dir='/tmp')
-
-    if cmd == 'start':
-        service.start()
-    elif cmd == 'stop':
-        service.stop()
-    elif cmd == 'status':
-        if service.is_running():
-            print "Service is running."
-        else:
-            print "Service is not running."
-    else:
-        sys.exit('Unknown command "%s".' % cmd)
+    main()
